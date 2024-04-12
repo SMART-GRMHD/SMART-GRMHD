36c2faa57e4c2103c15c16d8e0ec0bf3499dba05
Corwin <gmail@corwincheung.com>
2024-04-11 23:34:02 -0400

    adding to the gitignore file

--- src/plotting.py
+++ src/plotting.py
@@ -1,10 +1,11 @@
 import collections
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
-import pandas as pd
-import numpy as np
 from utilities import ResPINN
+
 
 #Loading a Model
 
